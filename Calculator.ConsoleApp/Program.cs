052f234714fad16b53920ae3a606d3b51156c22d
Thomas Galliker <thomas@bluewin.ch>
2022-09-07 21:56:25 +0200

    Rename project, update target frameworks

---
+++ Calculator.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value1: ");
+            var value1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter value2: ");
+            var value2 = Convert.ToDecimal(Console.ReadLine());
+
+            var calculatorService = new CalculatorService();
+            var sum = calculatorService.Add(value1, value2);
+            Console.WriteLine($"Sum: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
